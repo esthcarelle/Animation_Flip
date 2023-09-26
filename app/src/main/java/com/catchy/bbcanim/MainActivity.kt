6493b1715944f5a574b635b181370216ae0ce79d
carrelle <esthcarelle@gmail.com>
2023-09-26 21:48:53 +0200

    Added a circular list

--- app/src/main/java/com/catchy/bbcanim/MainActivity.kt
+++ app/src/main/java/com/catchy/bbcanim/MainActivity.kt
@@ -3,13 +3,23 @@ package com.catchy.bbcanim
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.catchy.bbcanim.ui.theme.BBCAnimTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +32,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CircularList(listOf("1","2","3","4","5","6","7","8","9"))
                 }
             }
         }
     }
+}
+
+@Composable
+fun CircularList(items: List<String>) {
+    var currentIndex by remember { mutableStateOf(0) }
+
+    // Create a circular scrolling LazyColumn
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        items(items.size) { index ->
+            val adjustedIndex = (currentIndex + index) % items.size
+            CircularListItem(text = items[adjustedIndex])
+        }
+    }
+}
+
+@Composable
+fun CircularListItem(text: String) {
+    BasicTextField(
+        value = text,
+        onValueChange = { /* Handle text change if needed */ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    )
 }
 
 @Composable
